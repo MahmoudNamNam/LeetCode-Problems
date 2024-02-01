7c9ceeebb38c46164b2f5b402d3144063062ae14
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-02-01 14:10:30 +0200

    add ex in momoize

--- Day-4/memoize.js
+++ Day-4/memoize.js
@@ -11,3 +11,13 @@ function memoize(fn) {
     }
   };
 }
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+});
+
+console.log(memoizedFn(2, 3)); // 5
+console.log(memoizedFn(2, 3)); // 5
+console.log(memoizedFn(4, 5)); // 9
+console.log(callCount); // 2
