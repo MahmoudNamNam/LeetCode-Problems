cc72da718f4a76e1416256f7c5bb9a6461bb0aa1
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-02-01 13:47:34 +0200

    day4

---
+++ Day-4/memoize.js
@@ -0,0 +1,13 @@
+function memoize(fn) {
+  const cache = new Map();
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      const result = fn(...args);
+      cache.set(key, result);
+      return result;
+    }
+  };
+}
