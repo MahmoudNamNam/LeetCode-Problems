143e36b5aa5e76505daafbbd61b8751e070c6925
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-03-14 23:48:28 +0200

    Add Doc ^_^

--- python/Hash Table/Happy Number.py
+++ python/Hash Table/Happy Number.py
@@ -1,8 +1,18 @@
 class Solution(object):
     def isHappy(self, n):
         """
-        :type n: int
-        :rtype: bool
+        Determine if a number is happy.
+
+        A happy number is a number defined by the following process:
+        Starting with any positive integer, replace the number by the sum of the squares of its digits,
+        and repeat the process until the number equals 1 (where it will stay), or it loops endlessly
+        in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+        Args:
+            n (int): The number to check for happiness.
+
+        Returns:
+            bool: True if the number is happy, False otherwise.
         """
         def get_next(num):
             sum_of_squares = 0
@@ -18,6 +28,7 @@ class Solution(object):
             n = get_next(n)
 
         return n == 1
+
 
 
 # Another Solution
--- python/Hash Table/Roman to Integer.py
+++ python/Hash Table/Roman to Integer.py
@@ -1,22 +1,41 @@
-
 def romanToInt(s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        result = 0
-        prev_value = 0
-        
-        for char in s:
-            value = roman_numerals[char]
-            if value > prev_value:
-                result += value - 2 * prev_value
-            else:
-                result += value
-            prev_value = value
-        
-        return result
+    """
+    Convert a Roman numeral to an integer.
+
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D, and M.
+    Symbol       Value
+    I            1
+    V            5
+    X            10
+    L            50
+    C            100
+    D            500
+    M            1000
+
+    To convert a Roman numeral to an integer, we iterate through the string from left to right,
+    adding the value of each symbol to the result. If a symbol appears before a larger symbol, we
+    subtract its value instead.
+
+    Args:
+        s (str): The Roman numeral string to convert.
+
+    Returns:
+        int: The integer representation of the Roman numeral string.
+    """
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    
+    for char in s:
+        value = roman_numerals[char]
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+    
+    return result
+
 # Test cases
 romanToInt("MCMXCIIV")
 
