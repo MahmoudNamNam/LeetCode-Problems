2634cf967cccc15d6474b2a676ec1a335793158e
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-04-16 19:48:13 +0200

    add Test Case in Product Sales Analysis III

--- DB/pandas/Product Sales Analysis III.py
+++ DB/pandas/Product Sales Analysis III.py
@@ -2,6 +2,24 @@ import pandas as pd
 
 def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
     sales['rank'] = sales.groupby('product_id')['year'].rank(method='min',ascending = True)
-    res = sales.loc[sales['rank']==1,['product_id','year','quantity','price']]\
-             .rename(columns = {'year':'first_year'})
+    res = sales.loc[sales['rank']==1,['product_id','year','quantity','price']].rename(columns = {'year':'first_year'})
     return res
+
+sales_data = {
+    'sale_id': [1, 2, 7],
+    'product_id': [100, 100, 200],
+    'year': [2008, 2009, 2011],
+    'quantity': [10, 12, 15],
+    'price': [5000, 5000, 9000]
+}
+
+product_data = {
+    'product_id': [100, 200, 300],
+    'product_name': ['Nokia', 'Apple', 'Samsung']
+}
+
+sales = pd.DataFrame(sales_data)
+product = pd.DataFrame(product_data)
+
+print(sales_analysis(sales, product))
+
