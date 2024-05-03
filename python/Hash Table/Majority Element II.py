c70372e2055c29b96432f99378bf3afd1f8655d6
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-05-03 09:07:48 +0300

    Majority Element II

---
+++ python/Hash Table/Majority Element II.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import Counter
+from math import ceil
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        res=[]
+        mid = len(nums)/3
+        for k,v in cnt.items():
+            if v>mid:
+                res.append(k)
+        return res
+
+s = Solution()
+
+print(s.majorityElement([1]))
