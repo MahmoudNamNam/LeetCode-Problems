289b01f9bf49d6b1cf704752f29ae4ebf4a07ef1
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-05-07 11:26:47 +0300

    Add Two Numbers

--- python/Linked List/Add Two Numbers.py
+++ python/Linked List/Add Two Numbers.py
@@ -1,1 +1,30 @@
-import numpy as np
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        carryOver = 0
+        
+        while l1 or l2 or carryOver > 0:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            sm = v1 + v2 + carryOver
+            carryOver = sm // 10
+            sm %= 10
+            
+            current.next = ListNode(sm)
+            current = current.next
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
