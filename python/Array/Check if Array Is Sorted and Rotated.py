e3612ebfc908daa1e8c459812619871533a2e63c
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-07-30 15:07:41 +0300

    Check if Array Is Sorted and Rotated

--- python/Array/Check if Array Is Sorted and Rotated.py
+++ python/Array/Check if Array Is Sorted and Rotated.py
@@ -4,15 +4,13 @@ def check_rotated_sorted(nums):
     n = len(nums)
     
     for i in range(n):
-        x =nums[(i + 1) % n]
-        if nums[i] > x:
+        if nums[i] > nums[(i + 1) % n]:
             count_breaks += 1
         if count_breaks > 1:
             return False
             
     return True
 
-# Test examples
 print(check_rotated_sorted([3, 4, 5, 1, 2]))  # Output: True
 print(check_rotated_sorted([2, 1, 3, 4]))     # Output: False
 print(check_rotated_sorted([1, 2, 3]))        # Output: True
