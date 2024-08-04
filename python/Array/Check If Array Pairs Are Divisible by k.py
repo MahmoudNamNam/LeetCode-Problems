f3108ece96bc224d8652f0b27239d319d3031529
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-08-04 15:31:04 +0300

    Check If Array Pairs Are Divisible by k

---
+++ python/Array/Check If Array Pairs Are Divisible by k.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        # Calculate remainders and their frequencies
+        remainder_count = Counter(x % k for x in arr)
+        
+        # Check if the number of elements with remainder 0 is even
+        if remainder_count[0] % 2 != 0 :
+            return False
+        
+        # Check for each remainder if it has a corresponding remainder that sums to k
+        for r in list(remainder_count):
+            if r == 0:
+                continue
+            complement = (k - r) % k
+            if remainder_count[r] != remainder_count.get(complement, 0):
+                return False
+        
+        return True
