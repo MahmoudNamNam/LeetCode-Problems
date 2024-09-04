51ed6ebfef2e04f1703e0e9774e30630366c395b
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-04 06:51:15 +0300

    Minimum Value to Get Positive Step by Step Sum

--- python/Prefix Sum/Minimum Value to Get Positive Step by Step Sum.py
+++ python/Prefix Sum/Minimum Value to Get Positive Step by Step Sum.py
@@ -1,4 +1,3 @@
-from itertools import accumulate
 from typing import List
 
 class Solution:
@@ -23,8 +22,3 @@ print(sol.minStartValue([-3,2,-3,4,2]))
 print(sol.minStartValue([1,-2,-3]))
 
 
-
-
-class Solution:
-    def minStartValue(self, nums: List[int]) -> int:
-        return max(1, 1 - min(accumulate(nums)))
