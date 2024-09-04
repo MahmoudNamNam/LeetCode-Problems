dcc799f6ee5266945a216a63d000a257d0b2907a
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-04 07:54:22 +0300

    Sliding Window Maximum

---
+++ python/Sliding Window/Sliding Window Maximum.py
@@ -0,0 +1,29 @@
+
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Initialize the deque and the result list
+        deq = deque()
+        result = []
+        
+        for i in range(len(nums)):
+            # Remove elements not in the sliding window
+            if deq and deq[0] == i - k:
+                deq.popleft()
+            
+            # Remove elements smaller than the current element
+            while deq and nums[deq[-1]] < nums[i]:
+                deq.pop()
+            
+            # Add the current element at the end of the deque
+            deq.append(i)
+            
+            # The front of the deque is the largest element of the current window
+            if i >= k - 1:
+                result.append(nums[deq[0]])
+        
+        return result
+
