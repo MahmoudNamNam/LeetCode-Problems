bdc0b1ebda9c2fcd19b71c7b320226edf5f5296f
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-04 05:53:42 +0300

    Number of Ways to Split Array #3 ways

---
+++ python/Prefix Sum/Number of Ways to Split Array.py
@@ -0,0 +1,60 @@
+from typing import List
+
+
+# class Solution:
+#     def waysToSplitArray(self, nums: List[int]) -> int:
+#         cnt =0
+#         for i in range(1,len(nums)):
+#             if sum(nums[:i])>=sum(nums[i:]):
+#                 cnt += 1
+#         return cnt
+        
+#  O(N^2)
+
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        
+        # Compute the prefix sums
+        prefix_sum = [0] * n
+        prefix_sum[0] = nums[0]
+        for i in range(1, n):
+            prefix_sum[i] = prefix_sum[i - 1] + nums[i]
+        
+        total_sum = prefix_sum[-1]
+        valid_splits = 0
+        
+        # Check each possible split index
+        for i in range(n - 1):
+            left_sum = prefix_sum[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                valid_splits += 1
+        
+        return valid_splits
+# O(N)
+
+
+
+from typing import List
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        
+        total_sum = sum(nums)
+        left_sum = 0
+        valid_splits = 0
+        
+        for i in range(n - 1):
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                valid_splits += 1
+        
+        return valid_splits
