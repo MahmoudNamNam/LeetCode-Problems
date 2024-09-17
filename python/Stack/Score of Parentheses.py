68c44b1f9ba3d4f28f4011a0acf044942a68ef3f
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-17 14:05:55 +0300

    Score of Parentheses

--- python/Stack/Score of Parentheses.py
+++ python/Stack/Score of Parentheses.py
@@ -25,3 +25,25 @@ sol = Solution()
 
 print(sol.scoreOfParentheses(s = "()"))
 print(sol.scoreOfParentheses(s = "(()(()))"))
+
+"""
+Consider: (()(()))
+We know any valid sub-expression can be replaced with its value. Let's find smallest ones
+
+( () ( () )) ==> ( 1+ ( () )) ==> ( 1 ( 1 )) ==> (1 + 2) ==> (3) ==> 6
+We see that subexpressions () with value 1 and (()) with value 2 are part of a bigger expression (()(()))
+
+We can write inefficient code to keep find internal small expressions, but can we do smarter?
+
+Thinking in processing left to right: we either
+- have ( to indicate a new subexpression that will have ) in future
+- or ) to indicate there is a subexpression that is done now
+
+Stack can help us to do that
+- We find (, we add 0: it represents the initial sum of the internal subexpressions
+- We find ), we finish a subexpression value and accumulate to its parent
+
+It might now be easy to get idea, but its solution can be a similar thinking style in other stack problems
+
+
+"""
