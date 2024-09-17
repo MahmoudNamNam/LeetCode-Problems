6f117a75c75856e794038e28830bd9f3b5357fdb
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-17 13:32:31 +0300

    Remove All Adjacent Duplicates in String II with $

--- python/Stack/Remove All Adjacent Duplicates in String II.py
+++ python/Stack/Remove All Adjacent Duplicates in String II.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        stack = []
+        stack =  [['$', 0]]     # a placeholder to mark stack is empty. This eliminates the need to do an empty check later
         
         for c in s:
-            # If stack is not empty and the top character matches the current character
-            if stack and stack[-1][0] == c:
+            # If the top character matches the current character
+            if stack[-1][0] == c:
                 # Increment the count of the top character
                 stack[-1] = (c, stack[-1][1] + 1)
                 # If the count reaches k, pop the character from the stack
@@ -20,3 +20,11 @@ class Solution:
 sol =Solution()
 print(sol.removeDuplicates(s = "abcd", k = 2))
 print(sol.removeDuplicates(s = "deeedbbcccbdaa", k = 3))
+
+
+
+"""
+I have initialzed the stack with ['$', 0] placeholder. 
+I am using ``$`` because the input string will only have lower case alphabets. I did this to remove the stack empty check. 
+
+"""
