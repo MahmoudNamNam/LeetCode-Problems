0ee27772e27cb4e4759e5ddeeeecb052d802fc4e
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-09-20 18:51:42 +0300

    Next Greater Element II

---
+++ python/Stack/Next Greater Element II.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [-1] * n 
+        stack = []
+        
+        for i in range(2 * n):
+            num = nums[i % n] 
+            while stack and nums[stack[-1]] < num:
+                index = stack.pop()
+                result[index] = num
+            if i < n:
+                stack.append(i)
+        
+        return result
+
+
+
+sol = Solution()
+print(sol.nextGreaterElements([1,2,1]))
