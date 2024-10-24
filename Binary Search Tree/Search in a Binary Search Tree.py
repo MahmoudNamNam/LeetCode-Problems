dbde2d8252d0aa73786a632e114a273399b36063
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-10-24 13:36:13 +0300

    700. Search in a Binary Search Tree

--- Binary Search Tree/Search in a Binary Search Tree.py
+++ Binary Search Tree/Search in a Binary Search Tree.py
@@ -9,3 +9,21 @@ class Solution:
             return self.searchBST(root.left, val)
         else:
             return self.searchBST(root.right, val)
+
+
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root :
+            return None
+        while root:
+            if val == root.val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None
+
+
