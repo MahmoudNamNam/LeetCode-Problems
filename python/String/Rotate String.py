8ba037b7770a0e107e6edbbd4c240e1daf2aca45
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-11-03 05:15:30 +0200

    796. Rotate String

--- python/String/Rotate String.py
+++ python/String/Rotate String.py
@@ -1,13 +1,11 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        # Check if lengths are equal
         if len(s) != len(goal):
             return False
         
         # Concatenate s with itself to form all possible rotations
         doubled_s = s + s
         
-        # Check if goal is a substring of doubled_s
         return goal in doubled_s
 
 sol =Solution()
