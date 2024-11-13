202fed4fb6c807c5e407273a21ade1e21970d328
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-11-13 09:57:49 +0200

    2563. Count the Number of Fair Pairs

---
+++ python/Binary Search/Count the Number of Fair Pairs.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        
+        n = len(nums)
+        count = 0
+        
+        for i in range(n - 1):
+            min_val = lower - nums[i]
+            max_val = upper - nums[i]
+            
+            start = bisect.bisect_left(nums, min_val, i + 1, n)
+            end = bisect.bisect_right(nums, max_val, i + 1, n) - 1
+            
+            if start <= end:
+                count += end - start + 1
+        
+        return count
