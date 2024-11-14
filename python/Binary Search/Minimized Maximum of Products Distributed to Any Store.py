591ac4a16a8eb15d0eb90b4ec5414501409df4e6
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-11-14 10:10:38 +0200

    2064. Minimized Maximum of Products Distributed to Any Store

---
+++ python/Binary Search/Minimized Maximum of Products Distributed to Any Store.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def canDistribute(max_products: int) -> bool:
+            stores_needed = sum((quantity + max_products - 1) // max_products for quantity in quantities)
+            print(stores_needed)
+            return stores_needed <= n
+
+        left, right = 1, max(quantities)
+        
+        while left < right:
+            mid = (left + right) // 2
+            if canDistribute(mid):
+                right = mid  
+            else:
+                left = mid + 1 
+        
+        return left
+
+
+sol = Solution()
+print(sol.minimizedMaximum(n = 6, quantities = [11,6]))
