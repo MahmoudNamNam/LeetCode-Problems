81af7104e722452a639839dc185c154e0a88db31
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-11-27 13:37:25 +0200

    572. Subtree of Another Tree

---
+++ python/Binary Tree/Subtree of Another Tree.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSame(root, subRoot):
+            # Check if two trees are the same
+            if not root and not subRoot:
+                return True
+            if not root or not subRoot:
+                return False
+            if root.val != subRoot.val:
+                return False
+            return isSame(root.left, subRoot.left) and isSame(root.right, subRoot.right)
+        
+        def isSubtreeRec(root, subRoot):
+            if not root:
+                return False
+            # Check if the current root matches subRoot
+            if isSame(root, subRoot):
+                return True
+            #  check left and right subtrees
+            return isSubtreeRec(root.left, subRoot) or isSubtreeRec(root.right, subRoot)
+        
+        return isSubtreeRec(root, subRoot)
