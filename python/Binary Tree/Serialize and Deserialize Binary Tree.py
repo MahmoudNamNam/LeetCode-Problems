d88c757e5a575f1b357620a99a82eb3baabe838a
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-12-01 13:18:09 +0200

    297. Serialize and Deserialize Binary Tree

---
+++ python/Binary Tree/Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return "" 
+        queue= deque([root])
+        result= []
+        while queue:
+            node = queue.popleft()
+            if node:
+                result.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                result.append('#')
+        return ','.join(result)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data =='':
+            return None
+        nodes =data.split(',')
+        root = TreeNode(int(nodes[0]))
+        queue =deque([root])
+        idx=1
+        while queue:
+            parent=queue.popleft()
+            if nodes[idx] != "#":
+                parent.left =TreeNode(int(nodes[idx]))
+                queue.append(parent.left)
+            idx+=1
+            if nodes[idx] != "#":
+                parent.right =TreeNode(int(nodes[idx]))
+                queue.append(parent.right)
+            idx+=1
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
