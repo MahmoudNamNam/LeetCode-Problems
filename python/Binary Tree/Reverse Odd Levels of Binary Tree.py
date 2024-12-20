87e16c903a2b2275c4cd5de035e0df4db5a26bcf
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-12-20 14:07:54 +0200

    2415. Reverse Odd Levels of Binary Tree BFS

--- python/Binary Tree/Reverse Odd Levels of Binary Tree.py
+++ python/Binary Tree/Reverse Odd Levels of Binary Tree.py
@@ -25,3 +25,46 @@ class Solution:
             dfs(root.left, root.right, 1)
 
         return root
+
+#*-> BFS
+
+from collections import deque
+
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        queue = deque([root])
+        level = 0
+
+        while queue:
+            # Number of nodes in the current level
+            level_size = len(queue)
+            current_level_nodes = []
+
+            # Traverse the current level
+            for _ in range(level_size):
+                node = queue.popleft()
+
+                # Collect nodes in current level if it's odd
+                if level % 2:
+                    current_level_nodes.append(node)
+
+                # Add children to the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # Reverse node values at the odd level
+            if current_level_nodes:
+                values = [node.val for node in current_level_nodes]
+                values.reverse()
+                for i, node in enumerate(current_level_nodes):
+                    node.val = values[i]
+
+            # Move to the next level
+            level += 1
+
+        return root
