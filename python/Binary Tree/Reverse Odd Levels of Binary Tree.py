84645ff021b5f48c9cfe88b9df1326f253981498
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2024-12-20 14:01:43 +0200

    2415. Reverse Odd Levels of Binary Tree

--- python/Binary Tree/Reverse Odd Levels of Binary Tree.py
+++ python/Binary Tree/Reverse Odd Levels of Binary Tree.py
@@ -1,21 +1,27 @@
 from typing import Optional
 
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(left_child, right_child, lvl):
-            if left_child is None or right_child is None:
+        def dfs(left_child: Optional[TreeNode], right_child: Optional[TreeNode], level: int) -> None:
+            # Base case
+            if not left_child or not right_child:
                 return
-    
-            if lvl %2== 0:
-                left_child.val,right_child.val = right_child.val,left_child.val
-            
-            dfs(left_child.left , right_child.right , lvl+1)
-            dfs(left_child.right , right_child.left , lvl+1)
-        dfs(root.left,root.right,0)
+
+            # Swap values at odd levels
+            if level % 2 == 1:
+                left_child.val, right_child.val = right_child.val, left_child.val
+
+            # Recursively process the next level
+            dfs(left_child.left, right_child.right, level + 1)
+            dfs(left_child.right, right_child.left, level + 1)
+
+        if root:
+            dfs(root.left, root.right, 1)
+
         return root
