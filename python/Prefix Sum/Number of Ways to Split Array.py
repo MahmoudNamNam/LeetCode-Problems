5ffbc6e4d06c56fbfc438589cf0b02acf459b460
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2025-01-03 14:07:11 +0200

    2270. Number of Ways to Split Array

--- python/Prefix Sum/Number of Ways to Split Array.py
+++ python/Prefix Sum/Number of Ways to Split Array.py
@@ -47,13 +47,13 @@ class Solution:
         if n < 2:
             return 0
         
-        total_sum = sum(nums)
+        right_sum = sum(nums)
         left_sum = 0
         valid_splits = 0
         
         for i in range(n - 1):
             left_sum += nums[i]
-            right_sum = total_sum - left_sum
+            right_sum -= nums[i]
             if left_sum >= right_sum:
                 valid_splits += 1
         
