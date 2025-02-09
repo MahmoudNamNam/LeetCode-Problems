f1f97ee74b36ccc6943fb0e83f5ddb0c103f2790
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2025-02-09 18:40:10 +0200

    2364. Count Number of Bad Pairs

---
+++ python/Hash Table/Count Number of Bad Pairs.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        freq = dict()
+        good_pairs = 0
+
+        for i, num in enumerate(nums):
+            key = num - i
+            good_pairs += freq.get(key, 0)
+            freq[key] = freq.get(key, 0) + 1
+
+        n = len(nums)
+        return (n * (n - 1)) // 2 - good_pairs
+
+sol = Solution()
+print(sol.countBadPairs([4,1,3,3]))
+print(sol.countBadPairs([1,2,3,4,5]))
